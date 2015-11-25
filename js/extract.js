bafbf4974bb087388631c9f86384d7d91305d14c
cruzj <you@example.com>
2015-11-25 13:53:56 +0000

    menu fix on responsiveness

--- js/extract.js
+++ js/extract.js
@@ -9,19 +9,13 @@ $(document).ready(function() {
 		$('.automate').css('paddingTop', '100px');
 	}
 
-    $(window).scroll(function() {
-        if ($(window).scrollTop() >= 200) {
-        	//$('.header .navbar').css('background-color', 'rgba(200, 200, 200, 0.8 )');
-        	//$('.menu').css('padding', '0');
-        	//$(".logo").css("width", "80px");
-        } else {
-        	$('.header .navbar').css('background-color', 'rgba(255, 255, 255, .8)');
-        	//$('.menu').css('padding', '2em 0');
-        	$(".logo").css("width", "150px");
-        }
+  $(document).ready(function () {
+    $(".navbar-nav li a").click(function(event) {
+      $(".navbar-collapse").collapse('hide');
     });
+  });
 
-    $('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
